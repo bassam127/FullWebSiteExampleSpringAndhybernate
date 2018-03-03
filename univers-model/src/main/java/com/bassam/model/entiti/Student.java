480bca4948ead966035ff847d10a991366e3225f
bassam127 <beso.127.d@gmail.com>
2018-03-03 22:44:19 +0000

    add first dataModel

---
+++ univers-model/src/main/java/com/bassam/model/entiti/Student.java
@@ -0,0 +1,87 @@
+package com.bassam.model.entiti;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "STUDENT")
+public class Student {
+	@Id
+	@GeneratedValue
+	@Column(name = "Id")
+	private Integer id;
+	
+	@NotNull(message = "you have to specify first name")
+	@Column(name = "First_Name")
+	private String firstName;
+
+	@NotNull(message = "you have to specify last name")
+	@Column(name = "Last_Name")
+	private String lastName;
+
+	@NotNull(message = "you have to age")
+	@Min(value = 0, message = "minimum value is zero")
+	@Max(value = 100, message = "max value is 100")
+	@Column(name = "Age")
+	private Integer age;
+
+	@NotNull(message = "gender must be set")
+	@Column( name = "Gender")
+	private String gender;
+
+	public Student() {
+
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return firstName + lastName + age + gender;
+	}
+
+}
