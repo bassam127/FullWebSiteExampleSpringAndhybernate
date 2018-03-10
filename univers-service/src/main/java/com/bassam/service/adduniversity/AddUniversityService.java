5489ff5af4a4590662621466de7a179244391b58
bassam127 <beso.127.d@gmail.com>
2018-03-10 19:19:14 +0000

    add university service

---
+++ univers-service/src/main/java/com/bassam/service/adduniversity/AddUniversityService.java
@@ -0,0 +1,7 @@
+package com.bassam.service.adduniversity;
+
+import com.bassam.model.entiti.University;
+
+public interface AddUniversityService {
+ public void addUniversity(University universityDao);
+}
