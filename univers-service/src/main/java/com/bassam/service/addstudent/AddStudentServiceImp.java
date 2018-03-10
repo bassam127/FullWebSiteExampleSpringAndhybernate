cbbc3162c5cf907c2066067d93153f552cd1834b
bassam127 <beso.127.d@gmail.com>
2018-03-10 19:15:57 +0000

    add impl Service

---
+++ univers-service/src/main/java/com/bassam/service/addstudent/AddStudentServiceImp.java
@@ -0,0 +1,25 @@
+package com.bassam.service.addstudent;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bassam.model.entiti.Student;
+import com.bassam.repository.student.StudentRepository;
+
+@Service
+public class AddStudentServiceImp implements AddStudentService {
+
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	public void saveStudent(Student studentDAO) {
+		Student student = new Student();
+		student.setFirstName(studentDAO.getFirstName());
+		student.setLastName(studentDAO.getLastName());
+		student.setAge(studentDAO.getAge());
+		student.setGender(studentDAO.getGender());
+
+		studentRepository.save(student);
+	}
+
+}
