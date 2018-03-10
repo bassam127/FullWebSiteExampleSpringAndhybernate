624caa008d91e34698f6eca3ea546cc8eae9f5bf
bassam127 <beso.127.d@gmail.com>
2018-03-10 19:22:56 +0000

    create show all students service

---
+++ univers-service/src/main/java/com/bassam/service/showallstudents/ShowAllStudentService.java
@@ -0,0 +1,10 @@
+package com.bassam.service.showallstudents;
+
+import java.util.List;
+
+import com.bassam.model.entiti.Student;
+
+public interface ShowAllStudentService {
+	public List<Student> getAllStudens();
+
+}
