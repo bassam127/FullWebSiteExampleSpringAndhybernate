242add5509fa575c694c1f59251e7b9bf04bbfda
bassam127 <beso.127.d@gmail.com>
2018-05-13 10:32:44 +0100

    more changes in universityRepository

--- univers-repository/src/main/java/com/bassam/repository/university/UniversityRepository.java
+++ univers-repository/src/main/java/com/bassam/repository/university/UniversityRepository.java
@@ -4,13 +4,17 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.bassam.model.entiti.University;
 
 @Repository
 public interface UniversityRepository extends JpaRepository<University, Integer> {
-	
+
 	@Query("select u from University u order by u.universityName")
 	List<University> getAllUniversity();
+
+	@Query("select count(s) from Student s where s.university.id =:universityId")
+	Integer getNumberOfStudents(@Param("universityId") Integer universityId);
 }
